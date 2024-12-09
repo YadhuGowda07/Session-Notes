fe533a2a9bec49a239c771dc7008d1ee7d2f8314
Harsh Sharma <harshsharma20503@gmail.com>
2024-12-09 23:13:16 +0530

    made a basic to do still not functional

--- Projects/Basic Todo/src/App.jsx
+++ Projects/Basic Todo/src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleInputChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    }
+  };
+
+  const handleTaskCompletion = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index] = {
+      ...updatedTasks[index],
+      isCompleted: !updatedTasks[index].isCompleted,
+    };
+    setTasks(updatedTasks);
+  };
+
+  const handleClearCompleted = () => {
+    setTasks(tasks.filter((task) => !task.isCompleted));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="main_component">
+      <div className="header">
+        <h1>ToDo</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="main_body">
+        <div className="input">
+          <input
+            type="text"
+            value={newTask}
+            onChange={handleInputChange}
+            placeholder="What do you need to do?"
+          />
+          <button onClick={handleAddTask}>ADD</button>
+        </div>
+        <div className="tasklist">
+          {tasks.map((task, index) => (
+            <div key={index} className="task-item">
+              <input
+                type="checkbox"
+                checked={task.isCompleted}
+                onChange={() => handleTaskCompletion(index)}
+              />
+              <label>{task}</label>
+            </div>
+          ))}
+          <button className="clear-completed" onClick={handleClearCompleted}>
+            Clear Completed
+          </button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
